f325b78a12c9f0c5d3645f89ef6222912c26033f
Chirag Jain <cjain8060@gmail.com>
2022-01-07 19:20:50 +0530

    added alan ai api button and a basic testCommnad

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
 
-function App() {
+const alanKey =
+  "5a6d14a9e8da336b1c0577d766c61b0e2e956eca572e1d8b807a3e2338fdd0dc/stage";
+
+const App = () => {
+  useEffect(() => {
+    // will only run once on initialisation
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ command }) => {
+        if (command === "testCommand") {
+          alert("This code was executed");
+        }
+      },
+    });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Alan AI News Application</h1>
     </div>
   );
-}
+};
 
 export default App;
